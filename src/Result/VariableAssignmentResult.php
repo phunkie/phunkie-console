7afa16c0b817f3cef57a115bdb5fdfbf53cecefe
Marcello Duarte <marcello.duarte@gmail.com>
2017-02-11 11:53:34 +0000

    Unit prints nothing

--- src/Result/VariableAssignmentResult.php
+++ src/Result/VariableAssignmentResult.php
@@ -4,6 +4,7 @@ namespace PhunkieConsole\Result;
 
 use function Phunkie\Functions\show\showType;
 use function Phunkie\Functions\show\showValue;
+use Phunkie\Types\Unit;
 
 class VariableAssignmentResult extends Result
 {
@@ -12,6 +13,9 @@ class VariableAssignmentResult extends Result
         $varName = $this->getResult()->_1;
         $varValue = $this->getResult()->_2;
 
+        if ($varValue instanceof Unit) {
+            return "";
+        }
         return $formatter()['bold']("\$" . $varName) . ": " . $formatter()['magenta']($formatter()['bold'](showType($varValue))) .
         " = " . $formatter()['bold'](showValue($varValue));
     }
